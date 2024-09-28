45a390340c1eb5cef44db1e8ccfbad52c08e0087
Solaris-CXVII <aldencua2403@gmail.com>
2024-09-29 00:45:47 +0800

    add javascript dump test directory

---
+++ javascript_dump_test/javascript_test_1.js
@@ -0,0 +1,11 @@
+const myArray = ["I", "love", "chocolate", "frogs"];
+const madeAString = myArray.join(" ");
+console.log(madeAString);
+// returns 'I love chocolate frogs'
+
+const madeAnotherString = myArray.join();
+console.log(madeAnotherString);
+// returns 'I,love,chocolate,frogs'
+
+const madeYetAnotherString = myArray.join("");
+console.log(madeYetAnotherString);
