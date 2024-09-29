c73de0fd0ddb4d427ec801fb9a6720c230e56057
Solaris-CXVII <aldencua2403@gmail.com>
2024-09-30 02:53:16 +0800

    finish dom_test_3 per mdn docs

---
+++ javascript_dump_test/dom_test_3/script.js
@@ -0,0 +1,22 @@
+const names = [
+  "Chris",
+  "Li Kang",
+  "Anne",
+  "Francesca",
+  "Mustafa",
+  "Tina",
+  "Bert",
+  "Jada",
+];
+const para = document.createElement("p");
+
+// Add your code here
+
+const randomNames = Math.floor(math.random() * names.length);
+para.textContent = names[randomNames];
+
+// Don't edit the code below here!
+
+section.innerHTML = " ";
+
+section.appendChild(para);
