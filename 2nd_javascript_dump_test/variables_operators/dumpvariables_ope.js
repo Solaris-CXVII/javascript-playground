059191a9b514afa828b00138dd973e31e5ff8078
Solaris <aldencua2403@gmail.com>
2025-07-20 22:03:44 +0800

    dump take 2

---
+++ 2nd_javascript_dump_test/variables_operators/dumpvariables_ope.js
@@ -0,0 +1,32 @@
+let name = "John";
+const surname = "Malden";
+
+console.log(name);
+console.log(surname);
+
+name = "Jane";
+
+console.log(name + "" + surname);
+console.log(surname);
+
+console.log(23 + 97);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+
+console.log(a);
+
+a = a + 5;
+
+console.log(a);
+
+let b = 2 * a;
+
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+
+console.log(percentage);
